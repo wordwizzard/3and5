3bce70f51bcb48a05b86244876fcd37b314b243c
wordwizzard <41087146+wordwizzard@users.noreply.github.com>
2018-07-13 19:17:21 +0200

    Update 3and5.py
    The cleanest I could think to make the code, while still working :).

--- 3and5.py
+++ 3and5.py
@@ -1,13 +1,11 @@
-limit = 100
-i = 1
 
-for i in range(limit):
-    if (i % 3 == 0) & (i % 5 != 0):
-        print('fizz')
-    elif (i % 5 == 0) & (i % 3 != 0):
-        print('buzz')
-    elif (i % 3 == 0) & (i % 5 == 0) & (i != 0):
-        print('fizzbuzz')
-    else:
-        print(i)
+for i in range(1,100):
+    word = []
+    if i % 3 == 0:
+        word.append('fizz')
+    if i % 5 == 0:
+        word.append('buzz')
+    if len(word) == 0:
+        word.append(str(i))
+    print(''.join(word))
 
